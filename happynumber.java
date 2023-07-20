f6c08e8edc621ffbb7580ea5a289fbcc6e72bf49
ompi1010 <120009915+ompi1010@users.noreply.github.com>
2023-07-20 10:21:00 +0530

    Update happynumber.java

--- happynumber.java
+++ happynumber.java
@@ -1,53 +1,38 @@
-package java_lab;
-import java.util.Scanner;
-public class binarysearch
-{
-    public static void main(String[] args)
-    {
-        Scanner scan=new Scanner(System.in);
-        int low,mid,high;
-        System.out.println("enter the number of elements in the array\n");
-        int n;
-        n=scan.nextInt();
-        System.out.println("enter elements in sorted order");
-        int arr[]=new int[n];
-        int i;
-        for(i=0;i<n;i++)
-        {
-            arr[i]=scan.nextInt();
+import java.util.*;
+interface A{
+    interface B{
+        int check(int num);
+    }
+}
+class happy implements A{
+    public int check(int num){
+        int rem=0,sum=0;
+        while(num>0){
+            rem=num%10;
+            sum+=rem*rem;
+            num=num/10;
         }
-        System.out.println("enter the element to search\n");
-        int num;
-        num=scan.nextInt();
-        int result=bs(arr,num,0,n-1);
-        if(result==-1)
+        return sum;
+    }
+}
+public class happynum1{
+    public static void main(String [] args){
+        happy ob=new happy();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the num");
+        int num = sc.nextInt();
+        int val=num;
+        while( val!=1&&val!=4)
         {
-            System.out.println("element not found");
+            val=ob.check(val);
+        }
+        if(val==1)
+        {
+            System.out.println("happy number");
         }
         else{
-            System.out.println("element found at "+result);
+            System.out.println("not a happy number");
         }
     }
-    static int bs(int arr[],int num,int low,int high)
-    {
-        if(high>=low)
-        {
-            int mid=(low+high)/2;
-            if(arr[mid]==num)
-            {
-                return mid;
-            }
-            else if(arr[mid]>num)
-            {
-                return bs(arr,num,low,mid-1);
-            }
-            else 
-            {
-                return bs(arr,num,mid+1,high);
-            }
-        }
-        else{
-            return -1;
-        }
-    }
+
 }
